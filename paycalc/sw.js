af8aee3d036476bc5d33eacd381cef976d0f10c2
rankfile6787 <267648116+rankfile6787@users.noreply.github.com>
2026-04-11 01:41:09 -0500

    Create sw.js

---
+++ paycalc/sw.js
@@ -0,0 +1,50 @@
+const CACHE_NAME = "zone3-pay-calculator-v2";
+const APP_SHELL = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  const request = event.request;
+
+  if (request.method !== "GET") return;
+
+  event.respondWith(
+    caches.match(request).then(cached => {
+      if (cached) return cached;
+
+      return fetch(request)
+        .then(response => {
+          const cloned = response.clone();
+          caches.open(CACHE_NAME).then(cache => {
+            cache.put(request, cloned);
+          });
+          return response;
+        })
+        .catch(() => caches.match("./index.html"));
+    })
+  );
+});
